c437d2b9ebb08836b26e97d25215114a3839551a
Nino (Nutsa) Kharazishvili <nutsa1999@gmail.com>
2024-12-11 23:52:56 +0400

    added components

--- src/app/image-url.pipe.ts
+++ src/app/image-url.pipe.ts
@@ -5,6 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ImageUrlPipe implements PipeTransform {
   transform(value: string): string {
+    console.log(value.replace('node_modules/place-my-order-assets', './assets'))
     return value.replace('node_modules/place-my-order-assets', './assets');
+    
   }
 }
--- src/app/restaurant/restaurant.component.ts
+++ src/app/restaurant/restaurant.component.ts
@@ -1,99 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Restaurant } from './restaurant';
+import { ResponseData, RestaurantService } from './restaurant.service';
 
-const fakeRestaurants = [
-  {
-    name: 'Poutine Palace',
-    slug: 'poutine-palace',
-    images: {
-      thumbnail: 'node_modules/place-my-order-assets/images/4-thumbnail.jpg',
-      owner: 'node_modules/place-my-order-assets/images/3-owner.jpg',
-      banner: 'node_modules/place-my-order-assets/images/2-banner.jpg',
-    },
-    menu: {
-      lunch: [
-        {
-          name: 'Crab Pancakes with Sorrel Syrup',
-          price: 35.99,
-        },
-        {
-          name: 'Steamed Mussels',
-          price: 21.99,
-        },
-        {
-          name: 'Spinach Fennel Watercress Ravioli',
-          price: 35.99,
-        },
-      ],
-      dinner: [
-        {
-          name: 'Gunthorp Chicken',
-          price: 21.99,
-        },
-        {
-          name: 'Herring in Lavender Dill Reduction',
-          price: 45.99,
-        },
-        {
-          name: 'Chicken with Tomato Carrot Chutney Sauce',
-          price: 45.99,
-        },
-      ],
-    },
-    address: {
-      street: '230 W Kinzie Street',
-      city: 'Green Bay',
-      state: 'WI',
-      zip: '53205',
-    },
-    _id: '3ZOZyTY1LH26LnVw',
-  },
-  {
-    name: 'Cheese Curd City',
-    slug: 'cheese-curd-city',
-    images: {
-      thumbnail: 'node_modules/place-my-order-assets/images/2-thumbnail.jpg',
-      owner: 'node_modules/place-my-order-assets/images/3-owner.jpg',
-      banner: 'node_modules/place-my-order-assets/images/2-banner.jpg',
-    },
-    menu: {
-      lunch: [
-        {
-          name: 'Ricotta Gnocchi',
-          price: 15.99,
-        },
-        {
-          name: 'Gunthorp Chicken',
-          price: 21.99,
-        },
-        {
-          name: 'Garlic Fries',
-          price: 15.99,
-        },
-      ],
-      dinner: [
-        {
-          name: 'Herring in Lavender Dill Reduction',
-          price: 45.99,
-        },
-        {
-          name: 'Truffle Noodles',
-          price: 14.99,
-        },
-        {
-          name: 'Charred Octopus',
-          price: 25.99,
-        },
-      ],
-    },
-    address: {
-      street: '2451 W Washburne Ave',
-      city: 'Green Bay',
-      state: 'WI',
-      zip: '53295',
-    },
-    _id: 'Ar0qBJHxM3ecOhcr',
-  },
-];
+export interface Data {
+  value: Restaurant[];
+  isPending: boolean;
+}
 
 @Component({
   selector: 'pmo-restaurant',
@@ -101,11 +13,18 @@ const fakeRestaurants = [
   styleUrl: './restaurant.component.css',
 })
 export class RestaurantComponent implements OnInit {
-  restaurants: any[] = [];
+  restaurants: Data = {
+    value: [],
+    isPending: false,
+  };
+
+  constructor(private restaurantService: RestaurantService) {}
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.restaurants = fakeRestaurants;
-    }, 500);
+    this.restaurants.isPending = true;
+    this.restaurantService.getRestaurants().subscribe((res: ResponseData) => {
+      this.restaurants.value = res.data;
+      this.restaurants.isPending = false;
+    });
   }
 }
